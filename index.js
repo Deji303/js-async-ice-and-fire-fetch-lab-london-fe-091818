856a611b52e9edd83ae4e50ac179cf613d54deb2
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-06 18:48:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
-  fetch("")
+  fetch("https://anapioficeandfire.com/api/books").then(response => response.json()).then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
